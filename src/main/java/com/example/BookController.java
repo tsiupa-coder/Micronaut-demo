3a040433a4ec885c1380010568fe5930b58c8f55
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-06-24 12:46:33 +0300

    another test endpoints

--- src/main/java/com/example/BookController.java
+++ src/main/java/com/example/BookController.java
@@ -1,8 +1,18 @@
 package com.example;
 
+import io.micronaut.http.HttpMessage;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Put;
+import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.http.annotation.RequestBean;
 import jakarta.inject.Inject;
+import jdk.jfr.ContentType;
 
 import java.util.List;
 
@@ -17,14 +27,43 @@ public class BookController {
         service.initDataBase();
     }
 
-    @Get("/books")
+    @Get
     public List<Book> books() {
         return service.findAll();
     }
 
+    @Post
+    public HttpMessage create(Book book) {
+        service.create(book);
+
+        return HttpResponse.status(HttpStatus.CREATED);
+    }
+
+    @Delete("{id}")
+    public HttpMessage delete(@PathVariable Long id){
+
+        service.delete(id);
+
+        return HttpResponse.status(HttpStatus.NO_CONTENT);
+    }
+
+
+    @Put("{id}")
+    public HttpMessage update(@PathVariable Long id, @QueryValue String name) {
+
+        service.updateName(id, name);
+
+        return HttpResponse.status(HttpStatus.ACCEPTED);
+    }
 
     @Get("/fall")
     public void fall() {
         service.fall();
+    }
+
+    @Get(value = "/test")
+    public String test() {
+
+        return "HI";
     }
 }
--- src/main/java/com/example/BookService.java
+++ src/main/java/com/example/BookService.java
@@ -1,11 +1,14 @@
 package com.example;
 
 import io.micronaut.context.annotation.Primary;
+import io.micronaut.http.HttpMessage;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Primary
 @Singleton
@@ -14,6 +17,7 @@ public class BookService {
     @Inject
     BookRepository repository;
 
+    @Transactional
     public void initDataBase(){
 
         Book book = new Book();
@@ -22,17 +26,41 @@ public class BookService {
         repository.save(book);
     }
 
+    @Transactional
     public List<Book> findAll(){
 
         return (List<Book>) repository.findAll();
     }
 
-
+    @Transactional
     public Book fall(){
 
         return repository
                 .findById(null)
                 .orElseThrow((NoSuchElementException::new));
 
+    }
+
+    @Transactional
+    public void create(Book book) {
+        repository.save(book);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+
+        repository.deleteById(id);
+    }
+
+   @Transactional
+    public void updateName(Long id, String title) {
+
+        Optional<Book> book = repository.findById(id);
+
+        if(book.isPresent()) {
+            Book bookUpdated = book.get();
+            bookUpdated.setTitle(title);
+            repository.update(bookUpdated);
+        }
     }
 }
