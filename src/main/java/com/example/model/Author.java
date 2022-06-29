2300c8a2a188f3197006ae204ad2afcb8f728c00
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-06-29 09:51:43 +0300

    remove lombok, so far so good

--- src/main/java/com/example/model/Author.java
+++ src/main/java/com/example/model/Author.java
@@ -1,8 +1,6 @@
 package com.example.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
 import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.Column;
@@ -15,12 +13,27 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "authors")
 @BatchSize(size = 10)
-@Getter
-@Setter
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     @Column(name = "full_name")
     String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
